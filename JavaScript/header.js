68fb46c48b64f4b982ceacf8853294b3be894ca0
Robert <robertosalazarvasquezalexis@gmail.com>
2025-06-02 10:46:11 -0700

    Css Fixes and Some changes.

--- JavaScript/header.js
+++ JavaScript/header.js
@@ -1,19 +1,8 @@
-// function myFunction(){
-//     var x = document.getElementById("myLinks");
-//     if(x.style.display==="block"){
-//         x.style.display="none";
-//     }else{
-//         x.style.display="block";
-//     }
-// }
-
-var prevScrollpos = window.pageYOffset;
-window.onscroll=function(){
-    var currentScrollPos=window.pageYOffset;
-    if(prevScrollpos > currentScrollPos){
-        document.getElementById("topnav").style.top="0";
-    }else{
-        document.getElementById("topnav").style.top="-50px";
+  window.addEventListener('scroll', function () {
+    const header = document.querySelector('body .header');
+    if (window.scrollY > 50) {
+      header.classList.add('visible');
+    } else {
+      header.classList.remove('visible');
     }
-    prevScrollpos=currentScrollPos;
-}
+  });
